96e32d249a65ab2c91e2759569d4033c6f44a3e8
Gunder Rollufson <gunder.rollufson@gmail.com>
2025-08-17 15:57:30 +0200

    fix3

--- presence-api/src/scripts/register-commands.ts
+++ presence-api/src/scripts/register-commands.ts
@@ -6,7 +6,7 @@ const task = new SlashCommandBuilder()
   .setDescription('Server to-do')
   .addSubcommand(sc =>
     sc.setName('add')
-      .setDescription('Legg til oppgave i denne/valgt kanal')
+      .setDescription('Legg til oppgave')
       .addStringOption(o => o.setName('title').setDescription('Tittel').setRequired(true))
       .addStringOption(o => o.setName('notes').setDescription('Notater'))
       .addStringOption(o => o.setName('due').setDescription('YYYY-MM-DD HH:mm'))
@@ -16,19 +16,18 @@ const task = new SlashCommandBuilder()
     sc.setName('list')
       .setDescription('List oppgaver')
       .addStringOption(o => o.setName('filter').setDescription('open|claimed|done|all')
-        .addChoices(
-          { name: 'open', value: 'open' },
-          { name: 'claimed', value: 'claimed' },
-          { name: 'done', value: 'done' },
-          { name: 'all', value: 'all' },
-        )));
+        .addChoices({name:'open',value:'open'},{name:'claimed',value:'claimed'},{name:'done',value:'done'},{name:'all',value:'all'})));
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN!);
+const appId = process.env.DISCORD_CLIENT_ID!;
+const guildId = process.env.GUILD_ID;
 
 (async () => {
-  await rest.put(
-    Routes.applicationCommands(process.env.DISCORD_CLIENT_ID!),
-    { body: [task.toJSON()] },
-  );
-  console.log('Slash-kommandoer registrert.');
+  if (guildId) {
+    await rest.put(Routes.applicationGuildCommands(appId, guildId), { body: [task.toJSON()] });
+    console.log('Guild-kommandoer registrert:', guildId);
+  } else {
+    await rest.put(Routes.applicationCommands(appId), { body: [task.toJSON()] });
+    console.log('Globale kommandoer registrert');
+  }
 })();
